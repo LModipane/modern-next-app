c331ee19ba79dffb14ce77e19917d6c18d7be7ce
shaun <modipanesh@gmail.com>
2023-05-10 11:33:49 +0200

    styled navigation

--- src/app/Components/Navbar/index.tsx
+++ src/app/Components/Navbar/index.tsx
@@ -1,16 +1,28 @@
 'use client';
 import Link from 'next/link';
 import Image from 'next/image';
-import React from 'react';
+import React, { useState } from 'react';
 import { auth } from '@/firebase/client';
-import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { signOut } from 'firebase/auth';
+import { useAuthState, useSignInWithGoogle } from 'react-firebase-hooks/auth';
 
 function Navbar() {
-	const isLogin = false;
-	const [signInWithGoogle, userCred, loading, error] =
-		useSignInWithGoogle(auth);
-	console.log(userCred);
-
+	const [signInWithGoogle] = useSignInWithGoogle(auth);
+	const [user, isLoading] = useAuthState(auth);
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    
+    //styles objects  
+	const black_btn = {
+		backgroundColor: 'black',
+		color: 'white',
+		borderRadius: '60px',
+		padding: '10px 30px',
+		fontSize: '15px',
+	};
+	const menuItem = {
+		margin: '5px',
+		fontSize: '18px',
+	};
 	return (
 		<nav className="flex justify-between items-center mb-16 w-full">
 			<Link href="/" className="flex gap-2 items-center">
@@ -24,23 +36,18 @@ function Navbar() {
 				<p className="font-bold text-2xl md:block hidden">Promptopia</p>
 			</Link>
 			{/* Desktio navigations */}
-			<div className="sm:flex hidden">
-				{isLogin ? (
+			<div className="hidden md:block">
+				{user && !isLoading ? (
 					<div className="flex justify-between items-center gap-3 mx-24">
 						<Link
 							href="/create-prompt"
 							className="hidden md:block"
-							style={{
-								backgroundColor: 'black',
-								color: 'white',
-								borderRadius: '60px',
-								padding: '10px 30px',
-								fontSize: '15px',
-							}}>
+							style={black_btn}>
 							Create Post
 						</Link>
 						<button
 							className=""
+							onClick={() => signOut(auth)}
 							style={{
 								border: '1px solid black',
 								color: 'black',
@@ -52,37 +59,80 @@ function Navbar() {
 						</button>
 						<Link href="/profile">
 							<Image
-								src="/user-profile.svg"
+								src={`${user?.photoURL}`}
 								alt="user-profile-image"
 								width="40"
 								height="40"
-								className="rounded-full p-2"
-								style={{ border: '1px solid black' }}
+								className="rounded-full"
 							/>
 						</Link>
 					</div>
 				) : (
-					<>
-						<button onClick={() => signInWithGoogle()}>Sign In</button>
-					</>
+					<div className="">
+						{isLoading ? (
+							'Loading...'
+						) : (
+							<button onClick={() => signInWithGoogle()}>Sign In</button>
+						)}
+					</div>
 				)}
 			</div>
 
 			{/* mobile navigations */}
-			<div className="md:hidden flex m-3">
-				{isLogin ? (
-					<Image
-						src="/user-profile.svg"
-						alt="user-profile-image"
-						width="40"
-						height="40"
-						className="rounded-full p-2"
-						style={{ border: '1px solid black' }}
-					/>
+			<div className="md:hidden m-3">
+				{user && !isLoading ? (
+					<div className="">
+						<Image
+							src={`${user?.photoURL}`}
+							alt="user-profile-image"
+							width="40"
+							height="40"
+							className="rounded-full"
+							onClick={() => setIsMenuOpen(prev => !prev)}
+						/>
+						{isMenuOpen && (
+							<div
+								className=" m-2 bg-white drop-shadow-md flex flex-col gap-2 p-5 "
+								style={{
+									width: '200px',
+									position: 'absolute',
+									right: '0px',
+									border: '1px solid black',
+									borderRadius: '30px',
+								}}>
+								<Link
+									href="/profile"
+									onClick={() => setIsMenuOpen(false)}
+									style={menuItem}>
+									My Profile
+								</Link>
+								<Link
+									href="/create-prompt"
+									onClick={() => setIsMenuOpen(false)}
+									style={menuItem}>
+									Create Propmt
+								</Link>
+								<button
+									className="mt-5"
+									style={black_btn}
+									type="button"
+									onClick={() => {
+										setIsMenuOpen(false);
+										signOut(auth);
+									}}>
+									Sign Out
+								</button>
+							</div>
+						)}
+					</div>
 				) : (
-					<>
-						<button>Sign In</button>
-					</>
+					<div className="">
+						{isLoading ? (
+							'Loading...'
+						) : (
+							<button onClick={() => signInWithGoogle()}>Sign In</button>
+						)}
+					</div>
 				)}
 			</div>
 		</nav>
